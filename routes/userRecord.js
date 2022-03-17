5f68e12672e6cc0d843b03bd2050c8734d33c361
Leon Gottschild <leon-gottschild@t-online.de>
2022-03-17 15:48:15 +0100

    updadet: login credentials

--- routes/userRecord.js
+++ routes/userRecord.js
@@ -5,6 +5,7 @@ const userRoutes = express.Router();
 const dbo = require("../db/conn");
 
 // This section will help you get a list of all the records. READ
+
 userRoutes.route("/Users").get(function (req, res) {
     let db_connect = dbo.getDb();
     db_connect
