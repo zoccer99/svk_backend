4489d9b134d46cf3d773df5cf17f0dd5bcc77ffd
Leon Gottschild <leon-gottschild@t-online.de>
2022-03-17 15:35:31 +0100

    updadet: added new collection instead of new database

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-const ghHost = process.env.URL;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-app.use(require("./routes/UserRecord"));
+app.use(require("./routes/userRecord"));
 // get driver connection
 const dbo = require("./db/conn");
 
